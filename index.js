ce47504cda9d4d36ca636d4512bb4e6245c42d3c
no-name3 <hajatyanvahe@gmail.com>
2022-12-18 17:58:54 -0800

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
 
 const PORT = process.env.PORT || 3001;
 
+server.use(cors);
 server.use(router);
 
 server.listen(PORT, () => {
